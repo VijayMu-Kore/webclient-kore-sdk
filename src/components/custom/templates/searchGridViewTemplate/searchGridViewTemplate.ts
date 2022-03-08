1984581e75b97c717ccc3e86b20fea53359dd0be
RushivarTa-Kore <rushivar.takhur@kore.com>
2022-03-08 16:49:16 +0530

    search grid template changes done wrt chat sdk

--- src/components/custom/templates/searchGridViewTemplate/searchGridViewTemplate.ts
+++ src/components/custom/templates/searchGridViewTemplate/searchGridViewTemplate.ts
@@ -9,8 +9,9 @@ class SearchGridViewTemplate {
         me.helpersObj = helpers;
         if (msgData.message[0] && msgData.message[0].component && msgData.message[0].component?.payload && msgData.message[0].component?.payload?.template_type == 'searchGridTemplate') {
             if (!msgData.message[0].component.payload.helpers) {
-                msgData.message[0].component.payload['helpers'] = me.helpersObj;
-              }
+                msgData.message[0].component.payload['helpers'] = me.helpersObj.helpers;
+            }
+            msgData.message[0].component.payload['helpers'] = me.helpersObj.helpers;
             me.messageHtml = $(me.getTemplateString(msgData.message[0].component.payload.template_type)).tmpl(msgData.message[0].component.payload);
             me.bindEvents(me.messageHtml);
             return me.messageHtml;
@@ -24,9 +25,9 @@ class SearchGridViewTemplate {
     getTemplateString(type: any) {
         const searchGridTemplate = '<script type="text/x-jqury-tmpl">\
         {{if structuredData.length}}\
-<div class="title-text-heading {{if renderTitle}}display-block{{else}}display-none{{/if}}">${titleName}</div>\
-{{if gridLayoutType==="img_common"}}\
-<div class="template-4-grid-list mb-15">\
+        <div class="title-text-heading {{if renderTitle}}display-block{{else}}display-none{{/if}}">${titleName}</div>\
+        {{if gridLayoutType==="img_common"}}\
+  <div class="template-4-grid-list mb-15">\
     {{each(key, data) structuredData.slice(0, 5)}}\
     {{if isClickable == true}}\
     <div class="grid-data-item click-to-navigate-url faqs-shadow isClickable" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}" href="${data.url}" target="_blank">\
@@ -61,72 +62,71 @@ class SearchGridViewTemplate {
     </div>\
     {{/if}}\
     {{/each}}\
-    <div class="show-more-list {{if doc_count==0 || doc_count<6 || isLiveSearch || isSearch}}display-none{{/if}}" groupName="${groupName}" templateName="${templateName}" pageNumber="${pageNumber}" fieldName="${fieldName}">\
-        <div>Show more <img src="{{if devMode}}assets/web-kore-sdk/demo/{{/if}}images/show_more.png" height="6" width="10" /></div>\
-        {{/if}}\
-        {{if gridLayoutType==="img_large"}}\
-        <div class="template-5-grid-list mb-15">\
-            {{each(key, data) structuredData.slice(0, 5)}}\
-            {{if isClickable == true}}\
-            <div class="grid-data-item click-to-navigate-url faqs-shadow isClickable" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}" href="${data.url}" target="_blank">\
-                <div class="inner-content-list ">\
-                    <img src="${data.img}">\
-                </div>\
-            </div>\
-            {{/if}}\
-            {{if isClickable == false}}\
-            <div class="grid-data-item click-to-navigate-url faqs-shadow" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}">\
-                <div class="inner-content-list">\
-                    <img src="${data.img}">\
-                </div>\
-            </div>\
-            {{/if}}\
-            {{/each}}\
+</div>\
+{{/if}}\
+{{if gridLayoutType==="img_large"}}\
+<div class="template-5-grid-list mb-15">\
+    {{each(key, data) structuredData.slice(0, 5)}}\
+    {{if isClickable == true}}\
+    <div class="grid-data-item click-to-navigate-url faqs-shadow isClickable" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}" href="${data.url}" target="_blank">\
+        <div class="inner-content-list ">\
+            <img src="${data.img}">\
         </div>\
-        {{/if}}\
-        {{if gridLayoutType==="img_left"}}\
-        <div class="template-6-grid-list">\
-            {{each(key, data) structuredData.slice(0, 5)}}\
-            <div class="grid-data-item faqs-shadow isClickable" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}">\
-                <div class="inner-content-list {{if textAlignment==" center"}}text-center{{/if}}">\
-                    <div class="heading-main text-truncate one-line-height" title="${data.heading}">{{html helpers.convertMDtoHTML(data.heading)}}</div>\
-                    {{each(key, res) [0,1,2]}}\
-                    <div class="image-with-text">\
-                        <div class="img-block">\
-                            <img src="${data.img}" />\
-                        </div>\
-                        <div class="image-info two-line-description" title="${data.description}">{{html helpers.convertMDtoHTML(data.description)}}</div>\
-                    </div>\
-                    {{/each}}\
+    </div>\
+    {{/if}}\
+    {{if isClickable == false}}\
+    <div class="grid-data-item click-to-navigate-url faqs-shadow" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}">\
+        <div class="inner-content-list">\
+            <img src="${data.img}">\
+        </div>\
+    </div>\
+    {{/if}}\
+    {{/each}}\
+</div>\
+{{/if}}\
+{{if gridLayoutType==="img_left"}}\
+<div class="template-6-grid-list">\
+    {{each(key, data) structuredData.slice(0, 5)}}\
+    <div class="grid-data-item faqs-shadow isClickable" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}">\
+        <div class="inner-content-list {{if textAlignment=="center"}}text-center{{/if}}">\
+            <div class="heading-main text-truncate one-line-height" title="${data.heading}">{{html helpers.convertMDtoHTML(data.heading)}}</div>\
+            {{each(key, res) [0,1,2]}}\
+            <div class="image-with-text">\
+                <div class="img-block">\
+                    <img src="${data.img}" />\
                 </div>\
+                <div class="image-info two-line-description" title="${data.description}">{{html helpers.convertMDtoHTML(data.description)}}</div>\
             </div>\
             {{/each}}\
         </div>\
-        {{/if}}\
-        {{if gridLayoutType==="img_top"}}\
-        <div class="template-7-grid-list mb-15">\
-            {{each(key, data) structuredData.slice(0, 5)}}\
-            {{if isClickable == true}}\
-            <div class="grid-data-item  click-to-navigate-url faqs-shadow isClickable" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}" href="${data.url}" target="_blank">\
-                {{else}}\
-                <div class="grid-data-item  click-to-navigate-url faqs-shadow" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}">\
-                    {{/if}}\
-                    <div class="inner-content-list {{if textAlignment==" center"}}text-center{{/if}}">\
-                        <div class="main-img-block">\
-                            <img src="${data.img}">\
-                        </div>\
-                        {{if data?.heading?.length}}\
-                        <div class="heading- text-truncate one-line-height" title="${data.heading}">{{html helpers.convertMDtoHTML(data.heading)}}</div>\
-                        {{/if}}\
-                        {{if data?.description?.length}}\
-                        <div class="desc-text-info two-line-description" title="${data.description}">{{html helpers.convertMDtoHTML(data.description)}}</div>\
-                        {{/if}}\
-                    </div>\
+    </div>\
+    {{/each}}\
+</div>\
+{{/if}}\
+{{if gridLayoutType==="img_top"}}\
+<div class="template-7-grid-list mb-15">\
+    {{each(key, data) structuredData.slice(0, 5)}}\
+    {{if isClickable == true}}\
+    <div class="grid-data-item  click-to-navigate-url faqs-shadow isClickable" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}" href="${data.url}" target="_blank">\
+        {{else}}\
+        <div class="grid-data-item  click-to-navigate-url faqs-shadow" contentId="${data.contentId}" contentType="${data.sys_content_type}" id="${key}">\
+            {{/if}}\
+            <div class="inner-content-list {{if textAlignment=="center"}}text-center{{/if}}">\
+                <div class="main-img-block">\
+                    <img src="${data.img}">\
                 </div>\
-                {{/each}}\
+                {{if data?.heading?.length}}\
+                <div class="heading- text-truncate one-line-height" title="${data.heading}">{{html helpers.convertMDtoHTML(data.heading)}}</div>\
+                {{/if}}\
+                {{if data?.description?.length}}\
+                <div class="desc-text-info two-line-description" title="${data.description}">{{html helpers.convertMDtoHTML(data.description)}}</div>\
                 {{/if}}\
             </div>\
-            {{/if}}\
+        </div>\
+        {{/each}}\
+    </div>\
+    {{/if}}\
+        {{/if}}\
         </script>'
         if (type === 'searchGridTemplate') {
             return searchGridTemplate;
