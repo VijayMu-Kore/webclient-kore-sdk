1128392782fdaa153cfcf31ab1c74d9969853a47
KOREIT\rakesh.manda <rakesh.manda@kore.com>
2022-04-28 16:23:47 +0530

    emoji changes initial commit

--- examples/esm/chat/js/index_chat.js
+++ examples/esm/chat/js/index_chat.js
@@ -30,6 +30,8 @@ chatWindowInstance.installPlugin(new TtsSpeechPlugin());
 
 chatConfig.botOptions.koreAPIUrl = "https://qa-bots.kore.ai/api/";//temporary
 chatConfig.botOptions.API_KEY_CONFIG.KEY="57ea0771ed7a49f9b1c47c64bbc59914dbf9a236d09d4272a7526645199219f1st0f";
+chatConfig.botOptions.API_KEY_CONFIG.bootstrapURL="https://qa-bots.kore.ai/api/platform/websdk";
+chatConfig.botOptions.brandingAPIUrl = chatConfig.botOptions.koreAPIUrl +'websdkthemes/:appId/activetheme';
 chatWindowInstance.show(chatConfig);
 
 
