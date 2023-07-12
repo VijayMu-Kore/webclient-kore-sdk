09ece666db505b3a045f95267018061fcdbc7959
pavan ichapurapu <pavan.ichapurapu@kore.com>
2023-07-12 17:00:43 +0530

    firefox issue fixed

--- src/components/search/config/findly-config.ts
+++ src/components/search/config/findly-config.ts
@@ -3,19 +3,36 @@ let findlyConfig:any = {};
 let botOptionsFindly: any = {};
 botOptionsFindly.logLevel = "debug";
 var serverUrl = window.location.href;
-var paramUrl="searchassist-qa.kore.ai"
-if(serverUrl && (serverUrl.includes("https"))){ // for installer 
+var paramUrl="searchassist.kore.ai"
+var httpStart = 'https://';
+var wssUrl = "wss";
+if(serverUrl && (serverUrl.includes("https://") || serverUrl.includes("http://"))){ // for installer 
 // if(serverUrl && (serverUrl.includes(".kore.ai") || serverUrl.includes(".korebots.com"))){//for app, dev, qa, pilot, prod
-    paramUrl=serverUrl.split('/')[2]
+    paramUrl=serverUrl.split('/')[2];
+    if(serverUrl.includes("https://")){
+      httpStart = "https://";
+      wssUrl = "wss";
+    }else{
+      httpStart = "http://";
+      wssUrl = "ws";
+    }
 }  
 if(window?.JWT_OBJ && window?.JWT_OBJ?.koreAPIUrl){
   paramUrl=window.JWT_OBJ.koreAPIUrl.split("/")[2].split(':')[0];
+    if(window.JWT_OBJ.koreAPIUrl.includes("https://")){
+      httpStart = "https://";
+      wssUrl = "wss";
+    }else{
+      httpStart = "http://";
+      wssUrl = "ws";
+    }
 }
+
 botOptionsFindly.logLevel = 'debug';
 // botOptionsFindly.koreAPIUrl = "https://searchassist-qa.kore.ai/searchassistapi/";
-botOptionsFindly.koreAPIUrl = "https://"+paramUrl+"/searchassistapi/";
+botOptionsFindly.koreAPIUrl = httpStart+paramUrl+"/searchassistapi/";
 // botOptionsFindly.baseAPIServer = "https://searchassist-qa.kore.ai";
-botOptionsFindly.baseAPIServer = "https://"+paramUrl;
+botOptionsFindly.baseAPIServer = httpStart+paramUrl;
 function koreGenerateUUID() {
   console.info("generating UUID");
   var d = new Date().getTime();
@@ -48,18 +65,18 @@ botOptionsFindly.JWTUrl =
 botOptionsFindly.userIdentity = koreGenerateUUID(); // Provide users email id here
 // botOptionsFindly.userIdentity = 'vaishali.addala@kore.com';// Provide users email id here
 botOptionsFindly.botInfo = {
-  chatBot: "kore",
-  taskBotId: "st-33224823-6b08-58df-b4a9-26fae57cfca2",
+  chatBot: "debug_payload_test",
+  taskBotId: "st-ffec6f37-caec-51fb-bcb8-430a34a3a579",
 };
-botOptionsFindly.clientId = "cs-160dc715-3d4c-5098-b044-d46d9e8b5b73";
-botOptionsFindly.clientSecret = "QhBAiCkcTe1EBat2ZVcGvJ2MLmgHZ6D8t8Y+U9QNDIs=";
-botOptionsFindly.searchIndexID = "sidx-c3e130fb-d840-52bd-8797-d78f1b9dd04f";
+botOptionsFindly.clientId = "cs-9510d399-1f66-5dc6-a1c4-66282f5af266";
+botOptionsFindly.clientSecret = "P4KpMGTZr4qPxqX9tfuFJRk3Zmcuvb+ihYSRymQ+FyU=";
+botOptionsFindly.searchIndexID = "sidx-b14e41f1-d50a-5fc2-bbd2-3c89594b2f47";
 
 
 // To modify the web socket url use the following option
 // For Socket Connection
 botOptionsFindly.reWriteSocketURL = {
-  protocol: "wss",
+  protocol:  wssUrl,
   // hostname: 'searchassist-qa.kore.ai'
   hostname:paramUrl
 };
