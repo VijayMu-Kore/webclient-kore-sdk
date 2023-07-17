2c66a35e22fd336ce3efce55faab41f88973faee
pavan ichapurapu <pavan.ichapurapu@kore.com>
2023-07-17 17:35:14 +0530

    FLY-8301 issue fixed

--- dist/hot/hot-update.js
+++ dist/hot/hot-update.js
@@ -1,115 +1,150 @@
 self["webpackHotUpdatekore_web_sdk"]("esm",{
 
-/***/ "./src/components/search/config/findly-config.ts":
-/*!*******************************************************!*\
-  !*** ./src/components/search/config/findly-config.ts ***!
-  \*******************************************************/
+/***/ "./src/templatemanager/templates/snippetCitationTemplate/snippetCitationTemplate.ts":
+/*!******************************************************************************************!*\
+  !*** ./src/templatemanager/templates/snippetCitationTemplate/snippetCitationTemplate.ts ***!
+  \******************************************************************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-var _a;
-var findlyConfig = {};
-var botOptionsFindly = {};
-botOptionsFindly.logLevel = "debug";
-var serverUrl = window.location.href;
-var paramUrl = "searchassist.kore.ai";
-var httpStart = 'https://';
-var wssUrl = "wss";
-if (serverUrl && (serverUrl.includes("https://") || serverUrl.includes("http://"))) { // for installer 
-    // if(serverUrl && (serverUrl.includes(".kore.ai") || serverUrl.includes(".korebots.com"))){//for app, dev, qa, pilot, prod
-    paramUrl = serverUrl.split('/')[2];
-    if (serverUrl.includes("https://")) {
-        httpStart = "https://";
-        wssUrl = "wss";
+/* harmony import */ var _utils_helpers__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../utils/helpers */ "./src/utils/helpers.js");
+/* harmony import */ var _snippetCitationTemplate_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./snippetCitationTemplate.scss */ "./src/templatemanager/templates/snippetCitationTemplate/snippetCitationTemplate.scss");
+/* harmony import */ var _templates_feedBackFormTemplate_feedBackFormTemplate__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../templates/feedBackFormTemplate/feedBackFormTemplate */ "./src/templatemanager/templates/feedBackFormTemplate/feedBackFormTemplate.ts");
+
+
+
+var SnippetCitationTemplate = /** @class */ (function () {
+    function SnippetCitationTemplate() {
     }
-    else {
-        httpStart = "http://";
-        wssUrl = "ws";
-    }
-}
-if ((window === null || window === void 0 ? void 0 : window.JWT_OBJ) && ((_a = window === null || window === void 0 ? void 0 : window.JWT_OBJ) === null || _a === void 0 ? void 0 : _a.koreAPIUrl)) {
-    paramUrl = window.JWT_OBJ.koreAPIUrl.split("/")[2].split(':')[0];
-    if (window.JWT_OBJ.koreAPIUrl.includes("https://")) {
-        httpStart = "https://";
-        wssUrl = "wss";
-    }
-    else {
-        httpStart = "http://";
-        wssUrl = "ws";
-    }
-}
-botOptionsFindly.logLevel = 'debug';
-// botOptionsFindly.koreAPIUrl = "https://searchassist-qa.kore.ai/searchassistapi/";
-botOptionsFindly.koreAPIUrl = httpStart + paramUrl + "/searchassistapi/";
-// botOptionsFindly.baseAPIServer = "https://searchassist-qa.kore.ai";
-botOptionsFindly.baseAPIServer = httpStart + paramUrl;
-function koreGenerateUUID() {
-    console.info("generating UUID");
-    var d = new Date().getTime();
-    if (window.performance && typeof window.performance.now === "function") {
-        d += performance.now(); //use high-precision timer if available
-    }
-    var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-        var r = (d + generateRandomNum() * 16) % 16 | 0;
-        d = Math.floor(d / 16);
-        return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
-    });
-    return uuid;
-}
-function generateRandomNum() {
-    var dateObj = new Date();
-    var month = dateObj.getUTCMonth() + 1;
-    var day = dateObj.getUTCDate();
-    var year = dateObj.getUTCFullYear();
-    var seconds = dateObj.getSeconds();
-    var minutes = dateObj.getMinutes();
-    var hour = dateObj.getHours();
-    var generatedNum = year * month * day * (hour + minutes * seconds);
-    return generatedNum;
-}
-botOptionsFindly.JWTUrl =
-    "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/users/sts";
-botOptionsFindly.userIdentity = koreGenerateUUID(); // Provide users email id here
-// botOptionsFindly.userIdentity = 'vaishali.addala@kore.com';// Provide users email id here
-botOptionsFindly.botInfo = {
-    chatBot: "debug_payload_test",
-    taskBotId: "st-ffec6f37-caec-51fb-bcb8-430a34a3a579",
-};
-botOptionsFindly.clientId = "cs-9510d399-1f66-5dc6-a1c4-66282f5af266";
-botOptionsFindly.clientSecret = "P4KpMGTZr4qPxqX9tfuFJRk3Zmcuvb+ihYSRymQ+FyU=";
-botOptionsFindly.searchIndexID = "sidx-b14e41f1-d50a-5fc2-bbd2-3c89594b2f47";
-// To modify the web socket url use the following option
-// For Socket Connection
-botOptionsFindly.reWriteSocketURL = {
-    protocol: wssUrl,
-    // hostname: 'searchassist-qa.kore.ai'
-    hostname: paramUrl
-};
-function clearLocalStorageUserDetails() {
-    window.localStorage.setItem("userName", "");
-    window.localStorage.setItem("userLocation", "");
-    window.localStorage.setItem("gender", "");
-    window.localStorage.setItem("userAge", "");
-}
-var favicon = document.getElementById("favicon");
-// CVS Caremark configs //
-botOptionsFindly.interface = "top-down";
-findlyConfig = {
-    botOptions: botOptionsFindly,
-    viaSocket: true,
-    pickersConfig: {
-        showDatePickerIcon: false,
-        showDateRangePickerIcon: false,
-        showClockPickerIcon: false,
-        showTaskMenuPickerIcon: true,
-        showradioOptionMenuPickerIcon: false, //set true to show Radio Option Template icon
-    },
-    API_KEY_CONFIG: { 'KEY': "YOUR_API_KEY" }
-};
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (findlyConfig);
+    SnippetCitationTemplate.prototype.renderMessage = function (msgData) {
+        var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m;
+        var me = this;
+        var $ = me.hostInstance.$;
+        var helpersObj = _utils_helpers__WEBPACK_IMPORTED_MODULE_1__["default"];
+        if (((_d = (_c = (_b = (_a = msgData === null || msgData === void 0 ? void 0 : msgData.message) === null || _a === void 0 ? void 0 : _a[0]) === null || _b === void 0 ? void 0 : _b.component) === null || _c === void 0 ? void 0 : _c.payload) === null || _d === void 0 ? void 0 : _d.template_type) === "citation_snippet") {
+            me.messageHtml = $(SnippetCitationTemplate.prototype.getTemplateString()).tmpl({
+                'snippetData': (_h = (_g = (_f = (_e = msgData === null || msgData === void 0 ? void 0 : msgData.message) === null || _e === void 0 ? void 0 : _e[0]) === null || _f === void 0 ? void 0 : _f.component) === null || _g === void 0 ? void 0 : _g.payload) === null || _h === void 0 ? void 0 : _h.snippetData,
+                'helpers': helpersObj.helpers,
+                'displayFeedback': (_m = (_l = (_k = (_j = msgData === null || msgData === void 0 ? void 0 : msgData.message) === null || _j === void 0 ? void 0 : _j[0]) === null || _k === void 0 ? void 0 : _k.component) === null || _l === void 0 ? void 0 : _l.payload) === null || _m === void 0 ? void 0 : _m.feedbackDisplay
+            });
+            me.feedBackTemplateObj = new _templates_feedBackFormTemplate_feedBackFormTemplate__WEBPACK_IMPORTED_MODULE_2__["default"]();
+            setTimeout(function () {
+                var _a, _b, _c, _d;
+                SnippetCitationTemplate.prototype.bindSnippetEvents(me, me.messageHtml, (_d = (_c = (_b = (_a = msgData === null || msgData === void 0 ? void 0 : msgData.message) === null || _a === void 0 ? void 0 : _a[0]) === null || _b === void 0 ? void 0 : _b.component) === null || _c === void 0 ? void 0 : _c.payload) === null || _d === void 0 ? void 0 : _d.snippetData);
+            }, 500);
+            return me.messageHtml;
+        }
+    };
+    SnippetCitationTemplate.prototype.getTemplateString = function () {
+        var snipppetCitationTemplate = '<script type="text/x-jqury-tmpl">\
+        <div class="search-temp-one">\
+        <div class="top-header">\
+            <div class="top-header-with-img">\
+                <span class="logo-span"><img src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/snippet-avathar.svg"/></span>\
+                <div class="btn-chip"><span class="sdk-i18n-lang" sdk-i18n-key="sa_sdk_Suggested_answer">{{html langTranslator("sa_sdk_Suggested_answer")}}</span>\</div>\
+            </div>\
+            {{if snippetData && snippetData.snippet_type === "generative_model"}}\
+            <div class="btn-link"><span class="bot-bg-purple"><img src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/bot.svg"/></span><span class="sdk-i18n-lang" sdk-i18n-key="sa_sdk_answered_by_ai">{{html langTranslator("sa_sdk_answered_by_ai")}}</span>\</div>\
+            {{/if}}\
+        </div>\
+        {{if snippetData && snippetData.title}}\
+        <div class="img-temp-title sa-sdk-title" data-title="{{html helpers.convertMDtoHTML(snippetData?.title)}}">{{html helpers.convertMDtoHTML(snippetData?.title)}}</div>\
+        {{/if}}\
+        <div class="citation-data-desc  {{if snippetData.title==""}}snippet_padding_top_0{{/if}}">\
+        {{each(key, data) snippetData.answer}}\
+        <span class="snippet-answer-fragment">{{html data.answer_fragment}}</span>{{each(sourceKey, source) data.sources}}<sup class="snippet-citation"><a href="${source.url}" target="_blank">[${source._id}]</a></sup>{{/each}}. </span>\
+        {{/each}}\
+        </div>\
+        <div class="snippet-referene-block">\
+          <div class="reference-block-header">References: </div>\
+          <ol type="1" class="reference-list-temp-ul">\
+                  {{each(key, item) snippetData.reference}}\
+                      <li class="reference-list-temp-li" title="{{html helpers.convertMDtoHTML(item.title)}}" ><a  href="${item.url}" target="_blank"><span>{{html helpers.convertMDtoHTML(item.title)}}</span></a></li>\
+                      {{/each}}\
+                  </ol>\
+        </div>\
+        {{if snippetData && snippetData.source}}\
+        <div class="snippet-source-block">\
+          <div class="snippet-source-file-name {{if !snippetData.source}} display-none{{/if}}">{{html snippetData.source}}</div>\
+          <a href="${snippetData?.page_url}" target="_blank" ><div class="snippet-source-url {{if !snippetData.page_url}} display-none{{/if}}"><span class="snippet-source-url-name" title="${snippetData?.page_url}">${snippetData?.page_url}</span><img src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/Icons/external-link.svg"/></div></a>\
+        </div>\
+        {{/if}}\
+        <div class="temp-footer-block">\
+            <div class="temp-footer {{if snippetData && snippetData.snippet_type !== "generative_model"}} justify-content-end {{/if}}">\
+                {{if snippetData && snippetData.snippet_type === "generative_model"}}\
+                <div class="btn-link"><span class="bot-bg-purple"><img src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/bot.svg"/></span><span class="sdk-i18n-lang" sdk-i18n-key="sa_sdk_answered_by_ai">{{html langTranslator("sa_sdk_answered_by_ai")}}</span>\</div>\
+                {{/if}}\
+                {{if displayFeedback}}\
+                <div class="temp-right">\
+                    <div class="is-it-usefull">Is it useful?</div>\
+                    <div class="temp-fotter-actions">\
+                        <img  class="snippet-feedback  snippet-like-img" src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/like-gray.svg" />\
+                        <img class="snippet-feedback  snippet-dislike-img" src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/dislike-gary.svg" />\
+                    </div>\
+                </div>\
+                <div id="snippet-feedback-template"></div>\
+                {{/if}}\
+            </div>\
+        </div>\
+    </div>\
+      </script>';
+        return snipppetCitationTemplate;
+    };
+    SnippetCitationTemplate.prototype.bindSnippetEvents = function (me, messageHtml, snippetData) {
+        var $ = me.hostInstance.$;
+        var hostInstance = me.hostInstance;
+        $(messageHtml).find('.temp-fotter-actions').off('click', '.snippet-like-img').on('click', '.snippet-like-img', function (event) {
+            if (!$(event.currentTarget).closest('.snippet-like-img').hasClass('active')) {
+                hostInstance.updateFeedBackResult('thumbsUp', snippetData.searchQuery, 'smartAnswer');
+                $(messageHtml).find('.snippet-feedback').removeClass('active');
+                $(event.currentTarget).addClass('active');
+            }
+        });
+        $(messageHtml).find('.temp-fotter-actions').off('click', '.snippet-dislike-img').on('click', '.snippet-dislike-img', function (event) {
+            if (!$(event.currentTarget).closest('.snippet-dislike-img').hasClass('active')) {
+                SnippetCitationTemplate.prototype.appendFeedBaackData(me, messageHtml, snippetData);
+                $(messageHtml).find('.snippet-feedback').removeClass('active');
+                $(event.currentTarget).addClass('active');
+            }
+        });
+        SnippetCitationTemplate.prototype.tooltipBindEvent(me);
+    };
+    SnippetCitationTemplate.prototype.tooltipBindEvent = function (me) {
+        var $ = me.hostInstance.$;
+        $('.sa-sdk-title').off('mouseover').on('mouseover', function (e) {
+            e.stopPropagation();
+            e.stopImmediatePropagation();
+            $(e.currentTarget).before('<div class="sdk-tooltip-container">' + $(e.currentTarget).attr('data-title') + '<span class="sa-tooltip-arrow"></span></div>');
+            $(e.currentTarget).parent().find('.sdk-tooltip-container').css('top', ($(e.currentTarget).position().top - ($(e.currentTarget).parent().find('.sdk-tooltip-container').height() + 25)) + 'px');
+        });
+        $('.sa-sdk-title').off('mouseout').on('mouseout', function (e) {
+            e.stopPropagation();
+            e.stopImmediatePropagation();
+            $(e.currentTarget).parent().find('.sdk-tooltip-container').remove();
+        });
+    };
+    SnippetCitationTemplate.prototype.appendFeedBaackData = function (me, messageHtml, snippetData) {
+        var $ = me.hostInstance.$;
+        var feedbackMsgData = {
+            message: [{
+                    component: {
+                        type: 'template',
+                        payload: {
+                            template_type: "feedbackFormTemplate",
+                            query: snippetData.searchQuery,
+                            feedBackType: 'smartAnswer'
+                        }
+                    }
+                }]
+        };
+        $(messageHtml).find('#snippet-feedback-template').empty().append(me.feedBackTemplateObj.renderMessage.bind(me, feedbackMsgData));
+    };
+    return SnippetCitationTemplate;
+}());
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SnippetCitationTemplate);
 
 
 /***/ })
@@ -118,7 +153,7 @@ findlyConfig = {
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("7ede5bc80372a770838b")
+/******/ 	__webpack_require__.h = () => ("228fe6e510f029ad8600")
 /******/ })();
 /******/ 
 /******/ }
