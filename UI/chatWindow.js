4b4dd7d51cf434af49e08dd2ade94cb4f87ad339
VenkateswararaoVe-Kore <venkateswara.velidi@kore.com>
2023-12-07 17:40:07 +0530

    scrolling issue fix for quick replies template

--- UI/chatWindow.js
+++ UI/chatWindow.js
@@ -1570,6 +1570,17 @@
                     if (type) {
                         type = type.toLowerCase();
                     }
+                    if (e.currentTarget.classList && e.currentTarget.classList.length > 0 && e.currentTarget.classList[0] === 'quickReply') {
+                        var _parentQuikReplyEle = e.currentTarget.parentElement.parentElement;
+                        var _leftIcon = _parentQuikReplyEle.parentElement.parentElement.querySelectorAll('.quickreplyLeftIcon');
+                        var _rightIcon = _parentQuikReplyEle.parentElement.parentElement.querySelectorAll('.quickreplyRightIcon');
+                        setTimeout(function () {
+                            _parentQuikReplyEle.parentElement.parentElement.getElementsByClassName('user-account')[0].classList.remove('marginT50');
+                            _parentQuikReplyEle.parentElement.parentElement.removeChild(_leftIcon[0]);
+                            _parentQuikReplyEle.parentElement.parentElement.removeChild(_rightIcon[0]);
+                            _parentQuikReplyEle.parentElement.removeChild(_parentQuikReplyEle);
+                        });
+                    }
                     if (type == "postback" || type == "text") {
                         $('.chatInputBox').text($(this).attr('actual-value') || $(this).attr('value'));
                         //var _innerText = $(this)[0].innerText.trim() || $(this).attr('data-value').trim();
@@ -1619,17 +1630,7 @@
                         $('.chatInputBox').text($(this).attr('title') +': '+ selectedValue.toString());
                         me.sendMessage($('.chatInputBox'),toShowText.toString());
                     }
-                    if (e.currentTarget.classList && e.currentTarget.classList.length > 0 && e.currentTarget.classList[0] === 'quickReply') {
-                        var _parentQuikReplyEle = e.currentTarget.parentElement.parentElement;
-                        var _leftIcon = _parentQuikReplyEle.parentElement.parentElement.querySelectorAll('.quickreplyLeftIcon');
-                        var _rightIcon = _parentQuikReplyEle.parentElement.parentElement.querySelectorAll('.quickreplyRightIcon');
-                        setTimeout(function () {
-                            _parentQuikReplyEle.parentElement.parentElement.getElementsByClassName('user-account')[0].classList.remove('marginT50');
-                            _parentQuikReplyEle.parentElement.parentElement.removeChild(_leftIcon[0]);
-                            _parentQuikReplyEle.parentElement.parentElement.removeChild(_rightIcon[0]);
-                            _parentQuikReplyEle.parentElement.removeChild(_parentQuikReplyEle);
-                        }, 50);
-                    }
+
                     setTimeout(function () {
                         var _chatInput = _chatContainer.find('.kore-chat-footer .chatInputBox');
                         _chatInput.focus();
